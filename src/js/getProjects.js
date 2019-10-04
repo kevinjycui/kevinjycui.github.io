774924200f7c226badeeb548805ec06c708ac7c1
Kevin Cui JY <kevincui1230@gmail.com>
2019-10-03 23:44:39 -0500

    updated ui

--- src/js/getProjects.js
+++ src/js/getProjects.js
@@ -1,6 +1,7 @@
 const portfolio = [
-    {'name':'Hack the North 2019 MPb3ats', 'desc':'', 'source':'src/img/medium.jpg', 'repo':'https://github.com/kevinjycui/MPb3ats'},
-    {'name':'PennApps XX Hand Spoken OCR', 'desc':'', 'source':'src/img/handspoken.JPG', 'repo':'https://github.com/kevinjycui/hand-spoken-backend'},
+    {'name':'Geopoli w/ discord.py', 'desc':'', 'source':'src/img/geopoli.PNG', 'repo':'https://top.gg/bot/600823440802316288'},
+    {'name':'Hack the North 2019 MPb3ats', 'desc':'', 'source':'src/img/medium.jpg', 'repo':'https://devpost.com/software/mpbeats'},
+    {'name':'PennApps XX Hand Spoken OCR', 'desc':'', 'source':'src/img/handspoken.JPG', 'repo':'https://devpost.com/software/hand-spoken'},
     {'name':'RBC VoiceHacks Innovoicer w/ Rasa to Mycroft', 'desc':'', 'source':'src/img/mycroft.jpg', 'repo':'https://github.com/kevinjycui/innovoicer'},
     {'name':'ENFERI w/ Emotiv and Arduino', 'desc':'', 'source':'src/img/work-img.jpg', 'repo':'https://github.com/kevinjycui/enferi'},
     {'name':'Dijkstra Transportation Network', 'desc':'', 'source':'src/img/map_visual.jpg', 'repo':'https://github.com/kevinjycui/Dijkstra-Transportation-Network'},
