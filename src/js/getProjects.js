291fc1f8966fbddbd7f980b495067830db9bc1d4
Kevin Cui JY <kevincui1230@gmail.com>
2019-10-27 21:17:06 -0500

    Add description modules

--- src/js/getProjects.js
+++ src/js/getProjects.js
@@ -1,18 +1,113 @@
 const portfolio = [
     {'date':'2019'},
-    {'name':'Geopoli w/ discord.py', 'desc':'', 'source':'src/img/geopoli.PNG', 'repo':'https://top.gg/bot/600823440802316288'},
-    {'name':'Hack the North 2019 MPb3ats', 'desc':'', 'source':'src/img/medium.jpg', 'repo':'https://devpost.com/software/mpbeats'},
-    {'name':'PennApps XX Hand Spoken OCR', 'desc':'', 'source':'src/img/handspoken.JPG', 'repo':'https://devpost.com/software/hand-spoken'},
-    {'name':'RBC VoiceHacks Innovoicer w/ Rasa to Mycroft', 'desc':'', 'source':'src/img/mycroft.jpg', 'repo':'https://github.com/kevinjycui/innovoicer'},
-    {'name':'ENFERI w/ Emotiv and Arduino', 'desc':'', 'source':'src/img/work-img.jpg', 'repo':'https://github.com/kevinjycui/enferi'},
-    {'name':'Dijkstra Transportation Network', 'desc':'', 'source':'src/img/map_visual.jpg', 'repo':'https://github.com/kevinjycui/Dijkstra-Transportation-Network'},
-    {'name':'Human Anatomy Model w/ Unity', 'desc':'', 'source':'src/img/human.jpg', 'repo':'https://github.com/kevinjycui/human-anatomy-model'},
-    {'name':'BFS Pacman Clone', 'desc':'', 'source':'src/img/pacman.png', 'repo':'https://github.com/kevinjycui/pac-graph'},    
+    {'name':'Geopoli w/ discord.py', 'desc':'Geopolitical economic Discord bot, fun. Application using Discord and European Central Bank Exchange Rate API', 'source':'src/img/geopoli.PNG', 'repo':'https://top.gg/bot/600823440802316288'},
+    {'name':'Hack the North 2019 MPb3ats', 'desc':'MPb3ats takes an MP3 file as input, and creates a customised level for its accompanying rhythm game.', 'source':'src/img/medium.jpg', 'repo':'https://devpost.com/software/mpbeats'},
+    {'name':'PennApps XX Hand Spoken OCR', 'desc':'An innovative solution that combines the ease of writing with the beauty of an individual\'s unique handwriting.', 'source':'src/img/handspoken.JPG', 'repo':'https://devpost.com/software/hand-spoken'},
+    {'name':'RBC VoiceHacks Innovoicer w/ Rasa to Mycroft', 'desc':'The Innovoicer performs a set of voice command based actions that assist with the recording, reporting, and management of business meetings', 'source':'src/img/mycroft.jpg', 'repo':'https://mycroft.ai/blog/connecting-rasa-to-mycroft-a-guide/'},
+    {'name':'ENFERI w/ Emotiv and Arduino', 'desc':'Electroencephalographic Neuromuscular Facial Expression Recognition Interface, uses Arduino and Emotiv API to mimic facial expressions. Built for TEJ3U1 course, full marks.', 'source':'src/img/work-img.jpg', 'repo':'https://github.com/kevinjycui/enferi'},
+    {'name':'Dijkstra Transportation Network', 'desc':'Automated Transportation Network using Dijkstra\'s Algorithm.', 'source':'src/img/map_visual.jpg', 'repo':'https://github.com/kevinjycui/Dijkstra-Transportation-Network'},
+    {'name':'Human Anatomy Model w/ Unity', 'desc':'Model made playing around with Unity\'s ProBuilder asset; human digestive and respiratory systems', 'source':'src/img/human.jpg', 'repo':'https://github.com/kevinjycui/human-anatomy-model'},
+    {'name':'BFS Pacman Clone', 'desc':'Applying the Breadth First Search (BFS) algorithm to the classic arcade game "Pacman".', 'source':'src/img/pacman.png', 'repo':'https://github.com/kevinjycui/pac-graph'},    
     {'date':'2018'},
-    {'name':'EEG Cursor Controller w/ Emotiv', 'desc':'', 'source':'src/img/eeg.png', 'repo':'https://medium.com/@kevinjycui/of-mice-and-mind-creating-a-simple-eeg-cursor-control-application-7fcdad703d2f'},
-    {'name':'Trinidad Guppy Simulator', 'desc':'', 'source':'src/img/trinidad.png', 'repo':'https://github.com/kevinjycui/Trinidad-Guppies'},    
-    {'name':'Westmount C.I. Model UN', 'desc':'', 'source':'src/img/wcimun.JPG', 'repo':'https://wcimun.netlify.com/'},
+    {'name':'EEG Cursor Controller w/ Emotiv', 'desc':'An application for converting real-time synaptic commands using the Emotiv INSIGHT headgear to navigate the cursor on a screen.', 'source':'src/img/eeg.png', 'repo':'https://medium.com/@kevinjycui/of-mice-and-mind-creating-a-simple-eeg-cursor-control-application-7fcdad703d2f'},
+    {'name':'Trinidad Guppy Simulator', 'desc':'Evolution simulating model based off of natural and sexual selection experiments of the Trinidad Guppy.', 'source':'src/img/trinidad.png', 'repo':'https://github.com/kevinjycui/Trinidad-Guppies'},    
+    {'name':'Westmount C.I. Model UN', 'desc':'Website for my high school Model UN club', 'source':'src/img/wcimun.JPG', 'repo':'https://wcimun.netlify.com/'},
 ]
+
+function showDesc0() {
+    var popup = document.getElementsByClassName("popuptext");
+    popup[0].className = 'popuptext show';
+}
+
+function hideDesc0() {
+    var popup = document.getElementsByClassName("popuptext");
+    popup[0].className = 'popuptext';
+}
+function showDesc1() {
+    var popup = document.getElementsByClassName("popuptext");
+    popup[1].className = 'popuptext show';
+}
+
+function hideDesc1() {
+    var popup = document.getElementsByClassName("popuptext");
+    popup[1].className = 'popuptext';
+}
+function showDesc2() {
+    var popup = document.getElementsByClassName("popuptext");
+    popup[2].className = 'popuptext show';
+}
+
+function hideDesc2() {
+    var popup = document.getElementsByClassName("popuptext");
+    popup[2].className = 'popuptext';
+}
+function showDesc3() {
+    var popup = document.getElementsByClassName("popuptext");
+    popup[3].className = 'popuptext show';
+}
+function hideDesc3() {
+    var popup = document.getElementsByClassName("popuptext");
+    popup[3].className = 'popuptext';
+}
+function showDesc4() {
+    var popup = document.getElementsByClassName("popuptext");
+    popup[4].className = 'popuptext show';
+}
+function hideDesc4() {
+    var popup = document.getElementsByClassName("popuptext");
+    popup[4].className = 'popuptext';
+}
+function showDesc5() {
+    var popup = document.getElementsByClassName("popuptext");
+    popup[5].className = 'popuptext show';
+}
+function hideDesc5() {
+    var popup = document.getElementsByClassName("popuptext");
+    popup[5].className = 'popuptext';
+}
+function showDesc6() {
+    var popup = document.getElementsByClassName("popuptext");
+    popup[6].className = 'popuptext show';
+}
+
+function hideDesc6() {
+    var popup = document.getElementsByClassName("popuptext");
+    popup[6].className = 'popuptext';
+}
+function showDesc7() {
+    var popup = document.getElementsByClassName("popuptext");
+    popup[7].className = 'popuptext show';
+}
+function hideDesc7() {
+    var popup = document.getElementsByClassName("popuptext");
+    popup[7].className = 'popuptext';
+}
+function showDesc8() {
+    var popup = document.getElementsByClassName("popuptext");
+    popup[8].className = 'popuptext show';
+}
+function hideDesc8() {
+    var popup = document.getElementsByClassName("popuptext");
+    popup[8].className = 'popuptext';
+}
+function showDesc9() {
+    var popup = document.getElementsByClassName("popuptext");
+    popup[9].className = 'popuptext show';
+}
+function hideDesc9() {
+    var popup = document.getElementsByClassName("popuptext");
+    popup[9].className = 'popuptext';
+}
+function showDesc10() {
+    var popup = document.getElementsByClassName("popuptext");
+    popup[10].className = 'popuptext show';
+}
+function hideDesc10() {
+    var popup = document.getElementsByClassName("popuptext");
+    popup[10].className = 'popuptext';
+}
+
+let ind = 0;
 
 for (e of portfolio) {
     if ('name' in e) {
@@ -20,19 +115,64 @@ for (e of portfolio) {
         link.href = e.repo;
         link.target = '_blank';
         let profile = document.createElement("div");
-        profile.className = "eventModule";
+        profile.className = "eventModule popup";
+        let popup = document.createElement("span");
+        popup.className = "popuptext";
+        popup.append(document.createTextNode(e.desc));
         let head = document.createElement("h4");
         head.className = "event-name";
         head.append(document.createTextNode(e.name))
-        let subtext = document.createElement("a");
-        subtext.className = "event-descrip";
-        subtext.append(document.createTextNode(e.desc));
         profile.appendChild(head);
-        profile.appendChild(subtext);
+        profile.appendChild(popup);
         profile.style.backgroundImage = "url("+e.source+")";
+        if (ind === 0) {
+            profile.onmouseover = showDesc0;
+            profile.onmouseout = hideDesc0;
+        }
+        else if (ind === 1) {
+            profile.onmouseover = showDesc1;
+            profile.onmouseout = hideDesc1;
+        }
+        else if (ind === 2) {
+            profile.onmouseover = showDesc2;
+            profile.onmouseout = hideDesc2;
+        }
+        else if (ind === 3) {
+            profile.onmouseover = showDesc3;
+            profile.onmouseout = hideDesc3;
+        }
+        else if (ind === 4) {
+            profile.onmouseover = showDesc4;
+            profile.onmouseout = hideDesc4;
+        }
+        else if (ind === 5) {
+            profile.onmouseover = showDesc5;
+            profile.onmouseout = hideDesc5;
+        }
+        else if (ind === 6) {
+            profile.onmouseover = showDesc6;
+            profile.onmouseout = hideDesc6;
+        }
+        else if (ind === 7) {
+            profile.onmouseover = showDesc7;
+            profile.onmouseout = hideDesc7;
+        }
+        else if (ind === 8) {
+            profile.onmouseover = showDesc8;
+            profile.onmouseout = hideDesc8;
+        }
+        else if (ind === 9) {
+            profile.onmouseover = showDesc9;
+            profile.onmouseout = hideDesc9;
+        }
+        else if (ind === 10) {
+            profile.onmouseover = showDesc10;
+            profile.onmouseout = hideDesc10;
+        }
         link.appendChild(profile);
         let element = document.getElementById("event-dir");
         element.appendChild(link)
+        ind++;
     } else {
         let line = document.createElement("hr");
         let year = document.createElement("a");
