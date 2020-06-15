b6727c35af8b7e5b970eaea37a3986ec85ee40a7
kevinjycui <info@kevincui.dev>
2020-06-15 15:28:20 -0400

    Update socials

---
+++ src/js/socials.js
@@ -0,0 +1,16 @@
+const socialButton = document.getElementById('more-socials');
+socialButton.style.textDecoration = 'underline';
+socialButton.addEventListener('mouseover', () => {
+    socialButton.style.cursor = 'pointer';
+});
+socialButton.addEventListener('click', () => {
+    const otherSocials = document.getElementsByClassName('other-social');
+    if (otherSocials[0].style.display == 'inline') {
+        for (soc of otherSocials) soc.style.display = 'none';
+        socialButton.textContent = 'More socials';
+    }
+    else {
+        for (soc of otherSocials) soc.style.display = 'inline';
+        socialButton.textContent = 'Less socials';
+    }
+});
